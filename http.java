cc401eb744e62b3d7d9e5087d43017d69827e1d9
righTrash <definitionofrightrash@gmail.com>
2020-10-15 17:28:31 +0330

    prototype to do

---
+++ http.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.net.*;
+
+public class Client
+{
+	public static Socket socket;
+	public static DataInputStream input;
+	public static DataOutputStream out;
+	public static String stdNumber = "hello";
+	public static String url;
+	public static String address;
+
+
+	public void send(String methodName){}
+
+	public void response(){}
+
+	public static void main(String args[]){
+		system.out.println(this.stdNumber);
+	
+	
+	
+	}	
+	
+	
+
+}
